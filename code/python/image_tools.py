d26b7866f0225eed774a57c9590f3bd2c557d30d
TK <thomas.kundera@yahoo.fr>
2026-02-10 14:25:23 +0100

    Fit circles

--- code/python/image_tools.py
+++ code/python/image_tools.py
@@ -3,6 +3,9 @@ import logging
 import cv2
 import numpy as np
 
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
 def adjust_contrast(image, saturation_percentile=99):
     """
     Adjust image contrast to ensure ~1% of pixels saturate (255) in at least one channel.
@@ -27,3 +30,43 @@ def adjust_contrast(image, saturation_percentile=99):
     img_adjusted = np.clip(img_float, 0, 255).astype(np.uint8)
 
     return img_adjusted
+
+
+def find_sun(image,focal_length,min_radius,max_radius):
+    # Adjust contrast for longer focal lengths (>300mm) to enhance
+    # Sun brightness
+    # FIXME: we may want to do this for shorter focal lengths
+    if focal_length > 300:
+        image = adjust_contrast(image, saturation_percentile=99)
+
+    # Convert to grayscale and apply Gaussian blur
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray_blurred = cv2.GaussianBlur(gray, (3, 3), 0)
+
+    # Detect circle with HoughCircles
+    circles = cv2.HoughCircles(
+        gray_blurred,
+        cv2.HOUGH_GRADIENT,
+        dp=1,
+        minDist=60,
+        param1=100,
+        param2=30,
+        minRadius=min_radius,
+        maxRadius=max_radius
+    )
+
+    # Process and draw the best fit circle
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        best_circle = circles[0][0]
+        x, y, r = best_circle
+        logging.info(f"Circle found: x={x}, y={y}, r={r}, image shape: {gray.shape}, focal_length={focal_length}mm")
+    else:
+        logging.info(f"No circles found in {imgfile}")
+        r=None
+    # Draw the circle and center on the original image)
+    if (r is not None):
+        cv2.circle(image, (x, y), int(r), (0, 255, 0), 3)  # Green circle
+        cv2.circle(image, (x, y), 2, (255, 0, 0), 4)  # Red center
+
+    return image,r
