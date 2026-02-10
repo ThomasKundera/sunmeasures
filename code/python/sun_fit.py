1b3b813bf1429459761181838b6a387450940c24
TK <thomas.kundera@yahoo.fr>
2026-02-10 13:15:01 +0100

    Code to get data from an image

---
+++ code/python/sun_fit.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+import logging
+
+from exifer import ExifData
+
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+
+def sun_fit(imgfile):
+    logging.info("sun_fit(%s)", imgfile)
+    return
+
+def main():
+    print("FIXME: standalone tests?")
+    return
+
+if __name__ == "__main__":
+    main()
+
