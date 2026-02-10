dcc35fc31d2793a07ae9fabad26255405aa8288c
TK <thomas.kundera@yahoo.fr>
2026-02-10 12:51:30 +0100

    Exporting variables for python script

--- code/bash/run_analysis.sh
+++ code/bash/run_analysis.sh
@@ -3,9 +3,9 @@
 # Location of this script
 SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # Projectdir is two up
-PROJECTDIR="$( cd $SCRIPTDIR/../.. >/dev/null 2>&1 && pwd )"
+export PROJECTDIR="$( cd $SCRIPTDIR/../.. >/dev/null 2>&1 && pwd )"
 
-WORKDIR=$PROJECTDIR/output
+export WORKDIR=$PROJECTDIR/output
 mkdir -p $WORKDIR
 
 echo "Working in $WORKDIR"
