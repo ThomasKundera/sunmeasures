4285205fd16dc4e21752ee77ebb944cecf7ed36e
TK <thomas.kundera@yahoo.fr>
2026-02-10 12:31:38 +0100

    Script to run whole analysis

---
+++ code/bash/run_analysis.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Location of this script
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# Projectdir is two up
+PROJECTDIR="$( cd $SCRIPTDIR/../.. >/dev/null 2>&1 && pwd )"
+
+WORKDIR=$PROJECTDIR/output
+mkdir -p $WORKDIR
+
+echo "Working in $WORKDIR"
+
+cd $WORKDIR
+
+echo "Setting up python environment"
+python3 -m venv runenv
+source runenv/bin/activate
+
+echo "Installing requirements"
+pip install -r $PROJECTDIR/requirements.txt
+
+echo "Running analysis"
+python $PROJECTDIR/code/python/image_analysis.py
